899dac5fb63ccc6360b54561fbef1594daddb5f8
KetAveryH <khollingsworth@g.hmc.edu>
2023-06-05 10:52:37 -0700

    Testing Push

---
+++ testing/image_test/energyTestWindow.py
@@ -0,0 +1,40 @@
+import pyautogui
+import subprocess
+import win32gui
+
+def click_button(window_title, relative_x, relative_y):
+    # Find the window by its title
+    window_handle = win32gui.FindWindow(None, window_title)
+
+    if window_handle == 0:
+        print(f"Window '{window_title}' not found.")
+        return
+
+    # Get the window's position
+    window_rect = win32gui.GetWindowRect(window_handle)
+    window_x = window_rect[0]
+    window_y = window_rect[1]
+
+    # Calculate the absolute coordinates inside the window
+    x = window_x + relative_x
+    y = window_y + relative_y
+
+    # Move the mouse to the specified coordinates and click
+    pyautogui.moveTo(x, y)
+    pyautogui.click()
+
+# Set the title of the window you want to click within
+window_title = "UM25C PC Software V1.3"
+
+# Set the relative coordinates within the window
+relative_x = 125
+relative_y = 50
+
+# Click the button inside the window to start the testing process
+click_button(window_title, relative_x, relative_y)
+
+# Specify the path to your Python file
+python_file_path = "/path/to/your/python/file.py"
+
+# Run the Python file using subprocess
+subprocess.call(["python", python_file_path])
---
+++ testing/image_test/energyTesting.py
@@ -0,0 +1,20 @@
+import pyautogui
+import subprocess
+
+def click_button(x, y):
+    # Move the mouse to the specified coordinates and click
+    pyautogui.moveTo(x, y)
+    pyautogui.click()
+
+# Set the coordinates where the button is located on your screen
+button_x = 100
+button_y = 200
+
+# Click the button to start the testing process
+click_button(button_x, button_y)
+
+# Specify the path to your Python file
+python_file_path = "test_image_power.py"
+
+# Run the Python file using subprocess
+subprocess.call(["python", python_file_path])
