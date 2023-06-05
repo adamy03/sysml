ec9647d962492ed709d75b82ee3ee0bf598e3240
KetAveryH <khollingsworth@g.hmc.edu>
2023-06-05 10:54:13 -0700

    Testing Final

--- testing/image_test/energyTestWindow.py
+++ testing/image_test/energyTestWindow.py
@@ -22,19 +22,20 @@ def click_button(window_title, relative_x, relative_y):
     # Move the mouse to the specified coordinates and click
     pyautogui.moveTo(x, y)
     pyautogui.click()
+    pyautogui.click()
 
 # Set the title of the window you want to click within
 window_title = "UM25C PC Software V1.3"
 
 # Set the relative coordinates within the window
-relative_x = 125
-relative_y = 50
+relative_x = 225
+relative_y = 75
 
 # Click the button inside the window to start the testing process
 click_button(window_title, relative_x, relative_y)
 
 # Specify the path to your Python file
-python_file_path = "/path/to/your/python/file.py"
+python_file_path = "test_image_power.py"
 
 # Run the Python file using subprocess
 subprocess.call(["python", python_file_path])
