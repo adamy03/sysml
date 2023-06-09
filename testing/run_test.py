bbc7da30d78d8236af9dafa792f73c4ea4ffaa75
Adam Yang <66138604+adamy03@users.noreply.github.com>
2023-06-09 11:04:05 -0700

    automatic logging

--- testing/run_test.py
+++ testing/run_test.py
@@ -18,4 +18,7 @@ def run_file(command):
     subprocess.run(command, shell=True)
 
 if __name__ == '__main__':
-    exec_file(SSH_PI4 + ' ' + "'python3 /home/pi/yolov5-on-rpi4-2020/johnston_yolov5/yolov5/detect.py --weights yolov5s.pt --conf 0.2 --source /home/pi/yolov5-on-rpi4-2020/johnston_yolov5/yolov5/images/shortVidIntersection.mp4'")
+    out = exec_file(SSH_PI3 + ' ' + "'python /home/pi/sysml/testing/sensing/video_test/taking_video_test.py'")
+
+    # Change file name/path as needed, assumes run_test is run from sysml directory
+    out.to_csv("./testing/test_results/auto_test.csv")
--- testing/sensor.py
+++ testing/sensor.py
@@ -2,12 +2,16 @@ import pyautogui
 import subprocess
 import win32gui
 import time
+import pandas as pd
 
 
 # Set the title of the window you want to click within
-WINDOW_TITLE = "UM25C PC Software V1.3"
-RELATIVE_X = 225
-RELATIVE_Y = 75
+UM25_WINDOW = 'UM25C PC Software V1.3'
+
+CONNECT_X = 225
+CONNECT_Y = 75
+GRAPH_X = 500
+GRAPH_Y = 75
 
 # Example for running image classification in pi3
 # command = "ssh pi@172.28.69.200 'python /home/pi/sysml/ModelClassification/testModel/image_classification_test.py'"
@@ -61,6 +65,39 @@ def click_button_end(window_title, relative_x, relative_y):
     pyautogui.moveTo(x, y)
     pyautogui.click()
 
+"""
+Returns graph of voltage, current, and power from UM25 software.
+"""
+def get_graph():
+    window_handle = win32gui.FindWindow(None, UM25_WINDOW)
+    
+    window_rect = win32gui.GetWindowRect(window_handle)
+    window_x = window_rect[0]
+    window_y = window_rect[1]
+
+    # Calculate the absolute coordinates inside the window
+    x = window_x + GRAPH_X
+    y = window_y + GRAPH_Y
+
+    # Move the mouse to the specified coordinates and click
+    pyautogui.moveTo(x, y)
+    pyautogui.click()
+    pyautogui.rightClick()
+
+    # Move to copy
+    x = x + 20
+    y = y + 30
+    pyautogui.moveTo(x, y)
+
+    # Copy to clipboard
+    pyautogui.click()
+
+    df = pd.read_clipboard()
+    df.drop(labels='Unnamed: 4', axis=1, inplace=True)
+    df['Power (W)'] = df['Voltage(V) - Voltage  graph'] * df['Current(A) - Current graph'] 
+
+    return df
+
 
 """
 Runs file on Pi from local machine, and begins UM25 logger. Note that logger
@@ -70,13 +107,15 @@ between the terminal and logger so the click will register on connect)
 def exec_file(command):
     start = time.time()
     print('starting sensor...')
-    click_button_start(WINDOW_TITLE, RELATIVE_X, RELATIVE_Y)
+    click_button_start(UM25_WINDOW, CONNECT_X, CONNECT_Y)
 
     print('running file...')
     subprocess.run(command, shell=True)
 
     print('stopping sensor')
-    click_button_end(WINDOW_TITLE, RELATIVE_X, RELATIVE_Y)
+    click_button_end(UM25_WINDOW, CONNECT_X, CONNECT_Y)
     print("exec file time: {}".format(time.time() - start))
+
+    return get_graph()
 
 
