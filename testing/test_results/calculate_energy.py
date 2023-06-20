38fd878287237dea357fba6329ee714d10007f0b
shivanim7 <smanivasagan@g.hmc.edu>
2023-06-20 09:12:43 -0700

    test results for vid res & yolov5n

--- testing/test_results/calculate_energy.py
+++ testing/test_results/calculate_energy.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 # Run from test_results directory
-FILE_PATH = "yolov5n/5sec_1080.csv"
+FILE_PATH = "yolov5n/5sec_480.csv"
 
 data = pd.read_csv(FILE_PATH)
 area = np.trapz(data['Power (W)'], data['Read times - Current graph'])
