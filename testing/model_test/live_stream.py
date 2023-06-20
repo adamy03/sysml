174f994c3d25ffa50e3d8121df45ac31e370e259
Adam Yang <66138604+adamy03@users.noreply.github.com>
2023-06-20 08:44:07 -0700

    functional VA pipeline

--- testing/model_test/live_stream.py
+++ testing/model_test/live_stream.py
@@ -46,9 +46,10 @@ out = cv2.VideoWriter(
 ####################################################################
 # INFERENCE #
 ####################################################################
-net = cv2.dnn.readNet('./models/yolov5s.onnx')
-with open('', 'r') as f:
-   class_names = f.read().split('\n')
+yolo_net = cv2.dnn.readNet('./models/yolov5s.onnx')
+class_list = []
+with open("./models/classes.txt", "r") as f:
+    class_list = [cname.strip() for cname in f.readlines()]
 
 # Define the torchvision image transforms
 def format_yolov5(frame):
@@ -58,6 +59,68 @@ def format_yolov5(frame):
     result[0:row, 0:col] = frame
 
     return result
+
+def yolo_infer(image):
+    input_image = format_yolov5(image) # making the image square
+    blob = cv2.dnn.blobFromImage(input_image , 1/255.0, (640, 640))
+    yolo_net.setInput(blob)
+    
+    outputs = yolo_net.forward()
+    output_data = outputs[0]
+
+    class_ids = []
+    confidences = []
+    boxes = []
+
+    image_width, image_height, _ = input_image.shape
+    x_factor = image_width / 640
+    y_factor =  image_height / 640
+
+    for r in range(25200):
+        row = output_data[r]
+        confidence = row[4]
+        if confidence >= 0.4:
+
+            classes_scores = row[5:]
+            _, _, _, max_indx = cv2.minMaxLoc(classes_scores)
+            class_id = max_indx[1]
+            if (classes_scores[class_id] > .25):
+
+                confidences.append(confidence)
+
+                class_ids.append(class_id)
+
+                x, y, w, h = row[0].item(), row[1].item(), row[2].item(), row[3].item() 
+                left = int((x - 0.5 * w) * x_factor)
+                top = int((y - 0.5 * h) * y_factor)
+                width = int(w * x_factor)
+                height = int(h * y_factor)
+                box = np.array([left, top, width, height])
+                boxes.append(box)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.25, 0.45) 
+
+    result_class_ids = []
+    result_confidences = []
+    result_boxes = []
+
+    for i in indexes:
+        result_confidences.append(confidences[i])
+        result_class_ids.append(class_ids[i])
+        result_boxes.append(boxes[i])
+
+    for i in range(len(result_class_ids)):
+
+        box = result_boxes[i]
+        class_id = result_class_ids[i]
+
+        cv2.rectangle(image, box, (0, 255, 255), 2)
+        cv2.rectangle(image, (box[0], box[1] - 20), (box[0] + box[2], box[1]), (0, 255, 255), -1)
+        cv2.putText(image, class_list[class_id], (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, .5, (0,0,0))
+    
+    return image
+
+
 
 ####################################################################
 # PIPELINE START #
@@ -78,10 +141,7 @@ while True:
 
     # Inference
     if frames % 10 == 0:
-        input_image = format_yolov5(image) # making the image square
-        blob = cv2.dnn.blobFromImage(input_image , 1/255.0, (640, 640), swapRB=True)
-        net.setInput(blob)
-        outputs = net.forward()
+        yolo_infer(image)
 
     out.write(image)
     frames += 1
@@ -90,7 +150,7 @@ while True:
     # if elapsed_time < frame_delay:
     #     time.sleep(frame_delay - elapsed_time)
     
-    if time.time() - exec_start > 7:
+    if frames > 50:
         break
 
 print(frames)
