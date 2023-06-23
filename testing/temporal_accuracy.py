29e531bc3e3f1f28efa7bf27e44ac13236347285
KetAveryH <khollingsworth@g.hmc.edu>
2023-06-23 11:04:39 -0700

    temporal accuracy file changes

--- testing/temporal_accuracy.py
+++ testing/temporal_accuracy.py
@@ -20,11 +20,61 @@ FILE_PATH = "~/sysml/testing/model_test/yolov5-on-rpi4-2020/johnston_yolov5/yolo
 ground_truth = []
 
 
-def get_predictions_list(file_location, num_files):
-    predictions_list = []
+"""
+Returns a list of dictionaries, one dictionary per frame that contains the
+bounding box coordinates and labels of the ground truth model output.
+"""
+def get_ground_truth_list(file_location, num_files):
+    gt_list = []
     # Loop through the files containing labels + bboxes for each frame
     for i in range(1, num_files+1):
         fname = file_location + "_" + str(i) + ".txt"
+        # Read data
+        df = pd.read_csv(fname, sep=' ', header=None)
+        cols = ['label', 'x-center', 'y-center', 'width', 'height']
+        df.columns = cols
+        
+        # Create FloatTensor containing boxes
+        bbox_cols = df[['x-center', 'y-center', 'width', 'height']]
+        boxes = torch.tensor(bbox_cols.values, dtype=torch.float32)
+        
+        # Create IntTensor containing labels
+        labels = torch.tensor(df['label'])
+        
+        # Append dict with boxes, labels, and scores to the list
+        frame_dict = {'boxes':boxes,
+                      'labels':labels,
+                      }
+        gt_list.append(frame_dict)
+        
+    return gt_list
+
+
+"""
+Returns a list of dictionaries, one dictionary per frame that contains the
+bounding box coordinates, labels, and scores of the model prediction.
+"""
+def get_predictions_list(file_location, num_files):
+    predictions_list = []
+    # Loop through the files containing labels + bboxes for each frame and if there is no bounding box data we will use the previous
+
+    # We will then start from frame1, and if 
+
+    # We will loop through, if the file contents are empty then we will compare
+    j=1
+    # Create a set of existing bounding box files
+
+    for i in range(1, num_files+1):
+
+        fname = file_location + "_" + str(i) + ".txt"
+        
+        # checks whether we have written a bounding box for a given frame
+        if not os.path.exists(fname):
+            print(f"no label for frame {i}: filling with frame {j}" )
+            fname = file_location + "_" + str(j) + ".txt"
+        else:
+            j=i
+
         # Read data
         df = pd.read_csv(fname, sep=' ', header=None)
         cols = ['label', 'x-center', 'y-center', 'width', 'height', 'conf-score']
@@ -38,31 +88,34 @@ def get_predictions_list(file_location, num_files):
         labels = torch.tensor(df['label'])
         
         # Create FloatTensor containing scores
-        # scores = torch.tensor(df['conf-score'])
+        scores = torch.tensor(df['conf-score'])
         
         # Append dict with boxes, labels, and scores to the list
         frame_dict = {'boxes':boxes,
                       'labels':labels,
-                      # 'scores':scores
+                      'scores':scores
                       }
         predictions_list.append(frame_dict)
         
-        #if (i%50 == 0):
-            #print(predictions_list)
-        
     return predictions_list
-        
 
+     
+"""
+Calculates mAP (Mean Average Precision) metric given ground truth and predictions
+"""
 def calculate_accuracy(ground_truth, prediction):
-    metric = MeanAveragePrecision(iou_type="bbox")
+    metric = MeanAveragePrecision(box_format='xywh', iou_type="bbox")
     metric.update(prediction, ground_truth)
     result = metric.compute()
     return result['map'].item()
 
 
 if __name__ == '__main__':
-    ground_truth = get_predictions_list(FILE_PATH + "/sparse_yolov5l_gt/labels/sparse", 200)
-    print(ground_truth)
     
-    #mAP = calculate_accuracy(ground_truth, predictions)
-    #print("mAP: ", mAP)
+    # Get lists for ground truth and predictions
+    ground_truth = get_ground_truth_list(FILE_PATH + "/sparse_yolov5l_gt/labels/sparse", 200)
+    preds = get_predictions_list(FILE_PATH + "/sparse_yolov5l_gt/labels/sparse", 200)
+    
+    # Calculate mAP score
+    mAP = calculate_accuracy(ground_truth, preds)
+    print("mAP: ", mAP)
