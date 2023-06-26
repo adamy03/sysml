456090fc2578537949377cf3b88cb21056f84780
Adam Yang <66138604+adamy03@users.noreply.github.com>
2023-06-26 10:27:59 -0700

    analysis

--- va_pipeline/run.py
+++ va_pipeline/run.py
@@ -1,15 +1,35 @@
 import cv2
 import torch
+import pandas as pd
 from PIL import Image
+import time
 
+
+print('start')
 # Model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5n')
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
-cap = cv2.VideoCapture('../../../samples/NY_sample.mp4')
+cap = cv2.VideoCapture('../samples/NY_sample.mp4')
+outputs = []
+frame_no = 1
+
+start = time.time()
 ret, frame = cap.read()
+while True:
 
-while cap.isOpened():
-        out = model(frame)
         ret, frame = cap.read()
+        if ret:
+                out = model(frame)
+                inf = out.pandas().xywh[0]
+                inf['frame'] = frame_no
+                outputs.append(inf)
+                print(out)
+        else:
+                break
+        frame_no += 1
+end = time.time()
 
-        print(out.pandas().xywh[0])
+print(f'runtime: {end - start}')
+print(f'frames: {frame_no - 1}')
+df = pd.concat(outputs)
+df.to_csv('../testing/test_results/yolov5s_NY.csv')
