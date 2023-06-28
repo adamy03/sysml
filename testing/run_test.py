8e0ace537d8e35061ccfe69c7cb50eb75ab22c36
Adam Yang <66138604+adamy03@users.noreply.github.com>
2023-06-28 14:59:27 -0700

    updates

--- testing/run_test.py
+++ testing/run_test.py
@@ -35,28 +35,30 @@ def calculate_stats(fpath, runtime):
 
 if __name__ == '__main__':
     # Change to name and path of output files
-    test_path = 'config_testing/sparse/' + 'sparse_yolov5n_1280_25fps'
+    TEST_DIR = './testing/test_results/config_testing/noisy/'
+    TEST_NAME = 'noisy_yolov5n_1280_720_25fps'
+    TEST_PATH = TEST_DIR + TEST_NAME
+
+    # Check for existing files:
+    matching_files = [filename for filename in os.listdir(TEST_DIR) if filename.startswith(TEST_NAME)]
+    print(matching_files)
+    assert len(matching_files) == 0, 'Test files already in directory.' 
 
     # Run test
     runtime, energy, out = exec_file(SSH_PI4 + ' ' + "'python ~/sysml/va_pipeline/run.py")
     subprocess.run('scp pi@172.28.81.58:' +
-                   '~/sysml/testing/test_results/temp.csv ./testing/test_results/' + 
-                   test_path + 
-                   '_inference.csv') #get model outputs
-    
-    
-    # Calculate statistics and save data
-   
-    energy.to_csv('testing/test_results/' + test_path + '_energy.csv')
-    energy, avg_power = calculate_stats('testing/test_results/' + test_path + '_energy.csv', runtime)
+                   '~/sysml/testing/test_results/temp.csv' + ' ' +
+                   TEST_PATH + 
+                   '_inference.csv'
+                   ) #get model outputs
 
-    with open('testing/test_results/' + test_path + '.txt', 'w') as file:
+    # Calculate statistics and save data
+    energy.to_csv(TEST_PATH + '_energy.csv')
+    energy, avg_power = calculate_stats(TEST_PATH + '_energy.csv', runtime)
+
+    with open(TEST_PATH + '.txt', 'w') as file:
         file.write(out.stdout + 
                    f'runtime (total): {runtime}\n'
                    f'energy: {energy}\n' +
                    f'avg power: {avg_power}'
                    )
-    
-    # print(energy, avg_power, out.stdout)
-
-    
