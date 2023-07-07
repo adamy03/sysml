6e742de9a02b8219d0b34475acac4088e3c7b5e1
KetAveryH <khollingsworth@g.hmc.edu>
2023-07-07 11:13:52 -0700

    test radom change

--- va_pipeline/tests.py
+++ va_pipeline/tests.py
@@ -12,12 +12,13 @@ file_names = os.listdir(folder_path)
 commands = []
 
 # Loop through all file names
-for conf in [0.3, 0.4, 0.5, 0.6, 0.7]:
-    for file_name in file_names:
-        # Form the command and add to the list
-        print(file_name)
-        command = f"python C:/Users/holli/sysml/va_pipeline/mod.py --img-width 1920 --img-height 1080 --yolov5-model yolov5x --conf {conf} --video-source \"{file_name}\""
-        commands.append(command)
+for res in [(1280, 720), (960, 540), (640, 360)]:
+    for conf in [0.3, 0.4, 0.5, 0.6, 0.7]:
+        for file_name in file_names:
+            # Form the command and add to the list
+            print(file_name)
+            command = f"python C:/Users/holli/sysml/va_pipeline/mod.py --img-width {res[0]} --img-height {res[1]} --yolov5-model yolov5n --conf {conf} --video-source \"{file_name}\""
+            commands.append(command)
 
 # Run each command using subprocess
 for command in commands:
