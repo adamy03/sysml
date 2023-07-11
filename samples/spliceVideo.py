3252654c455f6ccd2de6f20d0a43494c2e1714be
KetAveryH <khollingsworth@g.hmc.edu>
2023-07-11 13:41:53 -0700

    Video addition/name Change

--- samples/spliceVideo.py
+++ samples/spliceVideo.py
@@ -2,7 +2,7 @@ from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from moviepy.editor import VideoFileClip
 
 # Provide the path to the original video file
-video_path = "C:/Users/holli/sysml/samples/YouTube/4K Road traffic video for object detection and tracking - free download now! [MNn9qKG2UFI].mp4"
+video_path = "C:/Users/holli/sysml/samples/M6 Motorway Traffic [PNCJQkvALVc].mp4"
 video = VideoFileClip(video_path)
 duration = video.duration
 
@@ -18,10 +18,11 @@ for i in range(int(num_clips)):
     start_time = i * part_duration
     end_time = (i + 1) * part_duration
 
-    output_path = f"YouTube/Road_traffic_15/segments/part_{i+1}.mp4"  # Output file name for each part
+    output_path = f""  # Output file name for each part
 
     # Extract the subclip using ffmpeg_extract_subclip function from moviepy
-    if i == 12:
+
+    if i == 12: # Extracts 12th 15 second clip
         ffmpeg_extract_subclip(video_path, start_time, end_time, targetname=output_path)
 
     print(f"Part {i+1} extracted successfully.")
